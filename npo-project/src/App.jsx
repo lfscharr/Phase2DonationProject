30b7b0c3526f76d931f72f5c90e96130937c8a27
Lydia Scharron <lfscharr@gmail.com>
2023-10-16 17:42:29 -0600

    moved functionality up to App component

--- npo-project/src/App.jsx
+++ npo-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
@@ -15,18 +15,36 @@ import NpoPage from "./components/NpoPage";
 import FormLayout from "./layout/FormLayout";
 import NewNpoForm from "./components/NewNpoForm";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="new" element={<NewNpoForm />}>
-      </Route>
-    </Route>
-  )
-);
-
 function App() {
+  const [npos, setNpos] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/npos")
+      .then((res) => res.json())
+      .then((npoArray) => {
+        setNpos(npoArray);
+      });
+  }, []);
+
+  function handleAddNpo(newNpo) {
+    const updatedNpos = [...npos, newNpo];
+    setNpos(updatedNpos);
+  }
+
+  const displayNpos = npos.filter((npo) => {
+    return npo.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home npos={displayNpos} search={search} setSearch={setSearch}/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="new" element={<NewNpoForm handleAddNpo={handleAddNpo} />}></Route>
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 }
 
--- npo-project/src/pages/Home.jsx
+++ npo-project/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import NpoPage from "../components/NpoPage";
+import NpoList from "../components/NpoList";
+import Search from "../components/Search";
 
-function Home() {
+function Home({ npos, search, setSearch }) {
   return (
     <div>
-      <NpoPage />
+      <Search search={search} onSearch={setSearch} />
+      <NpoList npos={npos} />
     </div>
   );
 }
